f97524aa971c6eb928cdd5df7e89c32f3a0165da
walissonrubens <rubens.08p@gmail.com>
2023-11-05 14:22:32 -0300

    criei aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 80
+console.log(`sua velocidade é de ${vel}km/h`)
+if(vel > 60){ //condição simples
+    console.log('voce ultrapassou a velocidade permitida. MULTADO')
+}
+console.log('dirija sempre usando cinto de segurança')
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`vivendo em ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Brasileiro')
+} else{
+    console.log('Estrangeiro')
+}

a54081f538993fb531cd82bd0e905aae3f7d4887
walissonrubens <rubens.08p@gmail.com>
2023-12-12 19:40:58 -0300

    Update script.js

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,3 +1,6 @@
 function verificar(){
-    window.alert('ola')
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
 }
